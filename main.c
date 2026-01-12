cd7cde792ec8c05e8ade69e3ca913d5a7c24285d
Lucas Renan <lucaskoppfranca@gmail.com>
2026-01-12 04:33:34 +0000

    desisti transformei em double

--- main.c
+++ main.c
@@ -1,94 +1,137 @@
 #include <stdio.h>
 
-//transformando funcoes matematicas em codigo C
+//Copiando a biblioteca math usando apenas a stdlib
 
 
-int sqrt(){
-    
+//metodo babilonico pra descobrir a raiz quadrada
+int raizQuadrada(int numero){ //observe que a função trunca os valores para inteiros (pura conveniencia)
 
+    int chute_atual;
+    //gambiarra pq nao sei fazer do jeito certo
+    if (numero > 0 & numero < 1){
+        chute_atual = 1;
+    }
+    else{
+        chute_atual = numero / 2; 
+    }
 
+    for(int i = 0; i != 13; i++){
+        chute_atual = (chute_atual + numero / chute_atual) / 2;
+    }
 
+    return chute_atual; //valor da raiz mais proximo (truncado)
 }
 
 
-
 //funcao afim: f(x) = ax + b
-int funcaoAfim(){
+double funcaoAfim(){
 
     //coeficiente angular
-    int coef_angular;
+    dobule coef_angular;
 
     printf("Valor do coef angular ->\n");
     scanf("%d", &coef_angular);
 
     //coeficiente linear
 
-    int coef_linear = 0;
+    double coef_linear = 0;
 
     printf("Valor do coef linear ->\n"); 
     scanf("%d", &coef_linear);
 
     //valor de X
 
-    int valor_x = 0;
+    double valor_x = 0;
     printf("Digite o valor de X ->\n");
     scanf("%d", &valor_x);
 
     //calculo da funcao
 
-    int valor_final = coef_angular * valor_x + coef_linear;
+    dobule valor_final = coef_angular * valor_x + coef_linear;
     return valor_final;
 }
 
 
 //EQUAÇÃO DO SEGUNDO GRAU
 
+//struct das 2 raizes da eq do 2 grau
+typedef struct {
+    int raiz1;
+    int raiz2;
+} Raizes;
 
-int equacaoSegundoGrau(){
+Raizes equacaoSegundoGrau(double coef_quadratico, double coef_linear, double term_indep){
 
-    //coficiente quadrático (A)
-    int coef_quadratico;
-
-    printf("Valor do coeficiente quadrático (A) ->");
-    scanf("%d", &coef_quadratico);
-
-
-    //coeficiente linear (B)
-    int coef_linear;
-    printf("Valor do coeficiente quadrático (B) -> ");
-    scanf("%d", &coef_linear);
-
-
-    //termo independente (C)
-    int term_indep;
-    printf("Valor do termo independente ->");
-
-
-    //resolvendo classicamente por pitagoras pra facilitar
-
-    int delta = (coef_linear * coef_linear) + 4 * coef_quadratico * term_indep;
+    Raizes resultado;
+    //resolvendo por pitagoras pra facilitar e tratar delta == 0 
+    double delta = (coef_linear * coef_linear) - 4 * coef_quadratico * term_indep;
 
     if (delta < 0){
         printf("Equação sem solução real!");
-        return 0;
+        
+        resultado.raiz1 = 0;
+        resultado.raiz2 = 0;
+
+        return resultado;
     }
     
     //calculo do valor final da funcao
 
     if (delta == 0){
-        int valor_final = -coef_linear / 2 * coef_quadratico;
-        
-        return valor_final;
+        double valor_final = -coef_linear / 2 * coef_quadratico;
+
+        resultado.raiz1 = valor_final;
+        resultado.raiz2  = 0;
+
+        return resultado;
     }
     else{
-        int valor_final_um = -coef_linear + //esperando o metodo pra calcular raiz quadrada
 
+        //x1 -> sinal positivo
+        int x_um = (-coef_linear + raizQuadrada(delta)) / (2 * coef_quadratico);
+        
+        //x2 -> sinal negativo
+        int x_dois = (-coef_linear - raizQuadrada(delta)) / (2 * coef_quadratico);
+
+        resultado.raiz1 = x_um;
+        resultado.raiz2 = x_dois;
+
+        return resultado;
     }
+}
+
+
+//PRECISA DE CORREÇÕES 
+double exponencial(double numero, double intervalo){
+    //considerando que a funcao vai ter origem em 0
+
+    double termo = 1;
+    double soma = 1;
+    for (int i = 1; i != intervalo + 1; i++){
+            termo *= numero / i;
+            soma+= termo;
+        }
+    return soma;
+}
+
+
+//potenciação
+int potencia(int base, int expoente){
+    int resultado = 1;
+    for (int i = 0; i < expoente; i++){
+        resultado *= base;
+    }
+    return resultado;
 }
 
 
 int main(){
-    
-    
+
+    Raizes resultado = equacaoSegundoGrau(2, -8, 4);
+
+    printf("%d", resultado.raiz1);
+    printf("%d", resultado.raiz2);   
     return 0;
 }
+
+
